4efd57231c62fc6a2db2065151a190677a646bbc
peter.goostree <peter.goostree@mksinst.com>
2023-12-03 23:13:51 -0800

    Update readme and rename some files and methods

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# GameOfLife
-Console application that simulates the game of life
+# Conway's Game of Life
+
+## Introduction 
+
+net7.0 Console application that simulates [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life)
+
+# Getting Started
+
+ ### Software dependencies
+
+ - [.Net 7.0](https://dotnet.microsoft.com/en-us/download/dotnet/7.0)
+
+### From cmd line
+- `git clone https://github.com/PavoGrande/GameOfLife.git`
+- `cd .\GameOfLife`
+- `dotnet build .\src`
+
+## Run the App
+
+### From cmd line:
+
+- `dotnet run --project .\src\GameOfLife\GameOfLife.csproj`
---
+++ src/GameOfLife/LifeSim.cs
@@ -0,0 +1,116 @@
+﻿namespace GameOfLife
+{
+    /// <summary>
+    ///     Game of life simulation.
+    /// </summary>
+    public class LifeSim
+    {
+        private readonly bool[,] _cells;
+        private readonly int _height;
+        private readonly int _width;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="LifeSim"/> class.
+        /// </summary>
+        /// <param name="height">The height of the board.</param>
+        /// <param name="width">The width of the board.</param>
+        public LifeSim(int height, int width)
+        {
+            _height = height;
+            _width = width;
+            _cells = new bool[height, width];
+
+            GenerateBoard();
+        }
+
+        public void DrawAndGrow()
+        {
+            DrawGame();
+            Grow();
+        }
+
+        private void Grow()
+        {
+            for (var i = 0; i < _height; i++)
+            {
+                for (var j = 0; j < _width; j++)
+                {
+                    var numOfAliveNeighbors = GetNeighbors(i, j);
+
+                    if (_cells[i, j])
+                    {
+                        if (numOfAliveNeighbors < 2)
+                        {
+                            _cells[i, j] = false;
+                        }
+
+                        if (numOfAliveNeighbors > 3)
+                        {
+                            _cells[i, j] = false;
+                        }
+                    }
+                    else
+                    {
+                        if (numOfAliveNeighbors == 3)
+                        {
+                            _cells[i, j] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        private int GetNeighbors(int x, int y)
+        {
+            var aliveNeighbors = 0;
+
+            for (var i = x - 1; i < x + 2; i++)
+            {
+                for (var j = y - 1; j < y + 2; j++)
+                {
+                    if (!(i < 0 || j < 0 || i >= _height || j >= _width))
+                    {
+                        if (_cells[i, j])
+                        {
+                            aliveNeighbors++;
+                        }
+                    }
+                }
+            }
+
+            return aliveNeighbors;
+        }
+
+        private void DrawGame()
+        {
+            for (var i = 0; i < _height; i++)
+            {
+                for (var j = 0; j < _width; j++)
+                {
+                    Console.Write(_cells[i, j] ? "*" : " ");
+
+                    if (j == _width - 1)
+                    {
+                        Console.WriteLine("\r");
+                    }
+                }
+            }
+
+            Console.SetCursorPosition(0, Console.WindowTop);
+        }
+
+        private void GenerateBoard()
+        {
+            var random = new Random();
+
+            for (var i = 0; i < _height; i++)
+            {
+                for (var j = 0; j < _width; j++)
+                {
+                    var value = random.Next(2);
+                    _cells[i, j] = value != 0;
+                }
+            }
+        }
+    }
+}
